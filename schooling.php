91b5a56b2eec15f01be3f1452c9e075ee011a81f
Tim Hunt <timothyhunt1993@gmail.com>
2017-12-19 13:02:14 -0500

    data files

---
+++ schooling.php
@@ -0,0 +1,18 @@
+<div class="main">
+
+<i><h3>Kean University</h3></i>
+
+  <p><i style="font-weight:bold">Spring 2012</i>
+    <i>Met requirment to be on Deans List</i>
+  </p>
+
+
+
+<p><i style="font-weight:bold">May 2016</i>
+
+    <i>Graduated from Kean University with a Bacholars in Computer Science</i>
+  </p>
+
+
+
+</div>
---
+++ skills.php
@@ -0,0 +1,31 @@
+<div class="main">
+  <p>
+    <ul>
+      <li>Java
+
+
+      </li>
+      <li>HTML 5
+
+      </li>
+      <li>CSS 3
+
+
+      </li>
+      <li>Javascript(Jquery)
+
+
+      </li>
+      <li>PHP
+
+
+      </li>
+      <li>MySql(MySqli)
+
+
+      </li>
+    </ul>
+  </p>
+
+
+</div>
